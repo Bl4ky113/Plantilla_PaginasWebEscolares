04c45498b3ac5d2d718a9618b64a67c689b77bf2
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-01-22 17:48:59 -0500

    Agregado :hover a icono y function a dropMenu

--- src/public/js/header.js
+++ src/public/js/header.js
@@ -1,7 +1,15 @@
 // Change DropMenu Btn-icon Function
 function changeBtnIcon (btn) {
  btn.classList.toggle("btn-icon--active");
- 
 }
 
-//Check if js is working
+// Hide or Show DropMenu function 
+function showDropMenu () {
+ let dropMenu = document.getElementById("dropdownMenu");
+ dropMenu.classList.toggle("divMenu-dropMenu--visible")
+}
+
+//Check if header.JS is working
+
+//console.log("Hello");
+//alert("Hello");
